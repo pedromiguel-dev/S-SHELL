03a58163e7528885492ffd37f781125db7950da3
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-12-15 23:52:38 -0300

    builtins, renames, execute_f, launch_f

--- src/main.c
+++ src/main.c
@@ -1,10 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #define MAIN_SHELL_BUFSIZE 1024
 #define MAIN_SHELL_TOKEN_BUFFSIZE 64
+#define MAIN_SHELL_TOKEN_DELIM " \t\r\n\a\""
 
-char* main_shell_get_line () 
+/**
+ * sshel's built in functions
+*/
+int shell_main_cd(char **args);
+int shell_main_help(char **args);
+int shell_main_exit(char **args);
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int shell_main_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int (*builtin_func[]) (char **) = {
+  &shell_main_cd,
+  &shell_main_help,
+  &shell_main_exit
+};
+
+int shell_main_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "sshell: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0)
+        {
+            perror("sshell");
+        }
+    }
+    return 1;
+}
+
+int shell_main_help(char **args)
+{
+  int i;
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < shell_main_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int shell_main_exit(char **args)
+{
+  return 0;
+}
+
+/**
+ * sshel's inner workings
+*/
+
+char* shell_main_get_line () 
 {   
     int position = 0;
     int c;
@@ -42,9 +105,9 @@ char* main_shell_get_line ()
     return buffer;
 }
 
-char** main_shell_split_line(char* line)
+char** shell_main_split_line(char* line)
 {
-    const char* MAIN_SHELL_TOKEN_DELIM = " \t\r\n\a";
+    enum { OUTSIDE, INSIDE } state = OUTSIDE;
     int buffer_size = MAIN_SHELL_TOKEN_BUFFSIZE, position = 0;
     char** tokens = malloc(buffer_size * sizeof(char*));
     char* token;
@@ -55,11 +118,23 @@ char** main_shell_split_line(char* line)
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, MAIN_SHELL_TOKEN_DELIM);
+    token = strtok(line, MAIN_SHELL_TOKEN_DELIM );
     while (token != NULL)
     {
-        tokens[position] = token;
-        position++;
+        if (token != NULL && token[0] == '"') {
+            // Token starts with a double quote, move to INSIDE state
+            state = INSIDE;
+        } else {
+            tokens[position] = token;
+            position++;
+        }
+
+        if (state == INSIDE && token != NULL && token[strlen(token) - 1] == '"') {
+            // Token ends with a double quote, move back to OUTSIDE state
+            tokens[position] = token;
+            position++;
+            state = OUTSIDE;
+        }
 
         if (position >= buffer_size)
         {
@@ -78,10 +153,53 @@ char** main_shell_split_line(char* line)
     return tokens;
 }
 
-// TODO: launch shell commands function
-// TODO: execute function
+int shell_main_launch(char** args)
+{
+    pid_t pid, wpid;
+    int status;
 
-void main_shell_loop()
+    pid = fork();
+    if (pid == 0)
+    { // child
+        if ( execvp(args[0], args) == -1 )
+        {
+            perror("sshell exit child");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0)
+    {
+        perror("sshell fork failure");
+    } else { // parent
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED); 
+        } while (WIFEXITED(status) == NULL && WIFSIGNALED(status == NULL)); // parent will wait for child while is not errored or exited
+    }
+    return 1;
+}
+
+int shell_main_execute(char** args)
+{
+    int i;
+
+    if (args[0] == NULL)
+    {
+        return 1;
+    }
+
+    for (i = 0; i < shell_main_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+        
+    }
+    
+    return shell_main_launch(args);
+    
+}
+
+int shell_main_loop()
 {
     char* line;
     char** args;
@@ -89,18 +207,18 @@ void main_shell_loop()
 
     do {
         printf("simple_shell: ");
-        line = main_shell_get_line();
-        args = main_shell_split_line(line);
-
-        printf(line);
+        line = shell_main_get_line();
+        args = shell_main_split_line(line);
+        status = shell_main_execute (args);
 
         free(line);
         free(args);
     } while (status);
+
+    return status;
 }
 
 int main(int argc, char *argv[])
 {
-    main_shell_loop();
-    return 0;
+    return shell_main_loop();
 }
